67bfddaf47fc5b596a3ef652b378ac712f64aa6f
Your Name <your-email@example.com>
2024-01-04 13:46:51 +0000

    Update formula to version v0.0.54

--- codefly.rb
+++ codefly.rb
@@ -5,17 +5,17 @@
 class Codefly < Formula
   desc "codefly CLI"
   homepage "https://codefly.ai"
-  version "0.0.53"
+  version "0.0.54"
   depends_on :macos
   depends_on "buf"
   depends_on "protobuf"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/codefly-dev/cli-releases/releases/download/v0.0.53/codefly-darwin-arm64"
+      url "https://github.com/codefly-dev/cli-releases/releases/download/v0.0.54/codefly-darwin-arm64"
     end
     if Hardware::CPU.intel?
-      url  "https://github.com/codefly-dev/cli-releases/releases/download/v0.0.53/codefly-darwin-amd64"
+      url  "https://github.com/codefly-dev/cli-releases/releases/download/v0.0.54/codefly-darwin-amd64"
     end
 
     def install
